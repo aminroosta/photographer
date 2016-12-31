1ec702fad472302aac6c2b8addb4441c93dc08b5
amin roosta <amin.roosta@outlook.com>
2016-12-31 16:59:10 +0330

    add IRANsans fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {observer} from "mobx-react/native";
 import {
     StyleSheet,
     Text,
+    Button,
     View
 } from 'react-native';
 
@@ -12,17 +13,24 @@ interface Props { }
 interface State { }
 
 export default class App extends Component<Props, State> {
+    state = {
+      message: 'Hello'
+    };
+    add = () => {
+      this.state.message += '!';
+      this.setState(this.state);
+    }
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>
-                    Welcome to React Native!
+                    {this.state.message} Welcome to React Native!
                 </Text>
+                <Button title="me" onPress={this.add}></Button>
             </View>
         );
     }
 }
-
 
 const styles = StyleSheet.create({
     container: {
